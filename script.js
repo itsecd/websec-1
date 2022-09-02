1298ae1a55383e2eb8c790abf7a701b50ba8628a
Dryg1214 <pakazancev901@gmail.com>
2022-09-03 00:14:59 +0400

    Готова обработка ошибок

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 function buttonClickHandler(){
     let selectElem = document.getElementById("select1"); //действие
     
-    let value1 = parseFloat(document.getElementById("input1").value);
-    let value2 = parseFloat(document.getElementById("input2").value);
-    let resultStr = document.getElementById("input1").value + " " + selectElem.value + " " + document.getElementById("input2").value + " = ";
+    let value1 = document.getElementById("input1").value;
+    let value2 = document.getElementById("input2").value;
+
+    value1 = parseFloat(value1.replace(/[^\d.-]/g, ''));
+    value2 = parseFloat(value2.replace(/[^\d.-]/g, ''));
+
+    let resultStr = String(value1) + " " + selectElem.value + " " + String(value2) + " = ";
     let result = 0.0;
-    switch(selectElem.value){
-        case "+":
-            result = value1 + value2;
-            break;
-        case "-":
-            result = value1 - value2;
-            break;
-        case "*":
-            result = value1 * value2;
-            break;
-        case "/":
-            result = value1 / value2;
-            break;
+    let correct = true;
+
+    if (isNaN(value1) || isNaN(value2))
+    {
+        correct = false;
+        resultStr = "Не хватает данных, либо они некорректные";
     }
-    resultStr += result;
+
+    if (correct){
+        switch(selectElem.value){
+            case "+":
+                result = value1 + value2;
+                break;
+            case "-":
+                result = value1 - value2;
+                break;
+            case "*":
+                result = value1 * value2;
+                break;
+            case "/":
+                if(value2 !== 0){
+                    result = value1 / value2;
+                }
+                else{
+                    resultStr = "На ноль делить нельзя";
+                    result = "";
+                }
+                break;
+        }
+        
+        resultStr += result;
+    }
     document.getElementById("results").innerText = resultStr;
 }
