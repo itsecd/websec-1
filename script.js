e941ad94bd98a7b07089bd08a58abd9cc23a0715
PavelDenisov1 <paveldenisovqwe01@mail.ru>
2022-09-16 17:09:26 +0400

    Added input check function

--- script.js
+++ script.js
@@ -4,26 +4,37 @@ let value2 = document.getElementById("input2");
 let result = document.getElementById("result");
 
 function calculate() {
-    switch (option.value) {
-        case "sum":
-            res = parseFloat(value1.value) + parseFloat(value2.value);
-            result.innerText = res;
-            break;
-        case "sub":
-            res = parseFloat(value1.value) - parseFloat(value2.value);
-            result.innerText = res;
-            break;
-        case "mult":
-            res = parseFloat(value1.value) * parseFloat(value2.value);
-            result.innerText = res;
-            break;
-        case "div":
-            if (Math.abs(value2.value) < Number.EPSILON){
-                alert("Division by zero is not allowed!")
+
+    if(checkInput()) {
+        switch (option.value) {
+            case "sum":
+                res = parseFloat(value1.value) + parseFloat(value2.value);
+                result.innerText = res;
+                break;
+            case "sub":
+                res = parseFloat(value1.value) - parseFloat(value2.value);
+                result.innerText = res;
+                break;
+            case "mult":
+                res = parseFloat(value1.value) * parseFloat(value2.value);
+                result.innerText = res;
+                break;
+            case "div":
+                if (Math.abs(value2.value) < Number.EPSILON){
+                    alert("Division by zero is not allowed!")
+                    break;
+                }
+                res = parseFloat(value1.value) / parseFloat(value2.value);
+                result.innerText = res;
                 break;
             }
-            res = parseFloat(value1.value) / parseFloat(value2.value);
-            result.innerText = res;
-            break;
     }
+}
+
+function checkInput() {
+    if(value1.value == "" || value2.value == ""){
+        alert("Incorrect input.")
+        return false;
+    }
+    else return true;
 }
