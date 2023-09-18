88f5c97a8a7cf9d18987ebc993292e936505bca7
Sv163Sam <Tankruuus@gmail.com>
2023-09-18 19:09:02 +0400

    Add a new files html, css, js

---
+++ index.js
@@ -0,0 +1,49 @@
+function error(str) 
+{
+  alert(str + " incorrent");
+}
+
+function calc() {
+  let selectedElement = document.getElementById("select");
+
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let result = document.getElementById("lastresult").value;
+
+  if(isNaN(num1))
+  {
+    num2.onclick = error("First field");
+    document.getElementById("num1").focus();
+    return
+  }
+  if(isNaN(num2))
+  {
+    num2.onclick = error("Second field");
+    document.getElementById("num2").focus();
+    return
+  }
+
+  switch (selectedElement.value) {
+      case '+':
+          result = parseFloat(num1) + parseFloat(num2);
+          break;
+      case '-':
+          result = parseFloat(num1) - parseFloat(num2);
+          break;
+      case '*':
+          result = parseFloat(num1) * parseFloat(num2);
+          break;
+      case '/':
+          if (parseFloat(num2) != 0)
+              result = parseFloat(num1) / parseFloat(num2);
+          else
+              result = "Division by zero";
+          break;
+  }
+  if (!isNaN(result))
+      result = num1 + " " + selectedElement.value + " " + num2 + " " + "=" + " " + result;
+  
+  document.getElementById("memory1").innerHTML = document.getElementById("memory").innerHTML;
+  document.getElementById("memory").innerHTML = document.getElementById("lastresult").innerHTML;  
+  document.getElementById("lastresult").innerHTML = result + "<br/>";
+}
