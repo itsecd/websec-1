3fe2dd7fb293f811b6d2ee03dab6e14c310ee400
Нгок Хунг Фам <2020-03729@students.ssau.ru>
2023-09-21 23:19:54 +0400

    Лр 1 - Фам Нгок Хунг 6412

---
+++ script.js
@@ -0,0 +1,73 @@
+let history = [];
+
+function addHistory() {
+    let resultField = document.getElementById('result');
+    for (let caculation of history) {
+        let lableCaculation = document.createElement('lable');
+        lableCaculation.style.marginLeft = '10px';
+        lableCaculation.style.marginTop = '5px';
+        lableCaculation.textContent = caculation;
+        lableCaculation.style.fontSize = '20px';
+        lableCaculation.style.fontFamily = "Math";
+        lableCaculation.style.opacity = "0.5";
+        resultField.appendChild(lableCaculation);
+    }
+}
+
+function addNewElementHistory(newCalculation) {
+    history.push(newCalculation);
+}
+
+function scrollToBottom() {
+    var result = document.getElementById("result");
+    result.scrollTop = result.scrollHeight;
+}
+
+function isNumeric(inputString) {
+    return /^\d+$/.test(inputString);
+}
+
+function execute() {
+    let result = document.getElementById('result');
+    result.innerHTML = '';
+    addHistory();
+    try {
+        if (!isNumeric(document.getElementById('first_argument').value) || !isNumeric(document.getElementById('second_argument').value))
+            throw "Invalid input";
+
+        let firstArgument = parseInt(document.getElementById('first_argument').value);
+        let secondArgument = parseInt(document.getElementById('second_argument').value);
+        let operator = document.getElementById('operator');
+        let string = '';
+
+        if (isNaN(firstArgument) || isNaN(secondArgument)) {
+            throw "Invalid input";
+        }
+
+        if (operator.value == "addition") {
+            string = `${firstArgument} + ${secondArgument} = ${firstArgument + secondArgument}`;
+        }
+        else if (operator.value == "subtraction") {
+            string = `${firstArgument} - ${secondArgument} = ${firstArgument - secondArgument}`;
+        }
+        else if (operator.value == "multiplication") {
+            string = `${firstArgument} * ${secondArgument} = ${firstArgument * secondArgument}`;
+        }
+        else if (operator.value == "division") {
+            string = `${firstArgument} / ${secondArgument} = ${firstArgument / secondArgument}`;
+        }
+
+        let calculation = document.createElement('lable');
+        calculation.style.marginLeft = '10px';
+        calculation.style.marginTop = '5px';
+        calculation.textContent = string;
+        calculation.style.fontSize = '20px';
+        calculation.style.fontFamily = "Math";
+        result.appendChild(calculation);
+        scrollToBottom();
+        addNewElementHistory(string);
+    }
+    catch (ex) {
+        alert("Invalid input. Please enter valid numbers.");
+    }
+}
