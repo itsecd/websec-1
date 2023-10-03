2ed1471f17e1a475b11b531a93a53f87c9ced69f
YoniqueeZyzzFan <lolvan@mail.ru>
2023-10-03 22:33:41 +0400

    Calculator implemented

---
+++ calculator.js
@@ -0,0 +1,64 @@
+const history = [];
+
+function calculate() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const operator = document.getElementById("operator").value;
+    const resultElement = document.getElementById("result");
+    const historyElement = document.getElementById("history");
+
+    if (isNaN(num1) || isNaN(num2)) {
+        resultElement.textContent = "Error: Input correct numbers.";
+    } else {
+        let result;
+        let historyText = "";
+
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                historyText = `${num1} + ${num2} = ${result}`;
+                break;
+            case "-":
+                result = num1 - num2;
+                historyText = `${num1} - ${num2} = ${result}`;
+                break;
+            case "*":
+                result = num1 * num2;
+                historyText = `${num1} * ${num2} = ${result}`;
+                break;
+            case "/":
+                if (num2 === 0) {
+                    resultElement.textContent = "Error: Division by zero.";
+                    return;
+                }
+                result = num1 / num2;
+                historyText = `${num1} / ${num2} = ${result}`;
+                break;
+            default:
+                resultElement.textContent = "Error: Wrong operation.";
+                return;
+        }
+
+        resultElement.textContent = result;
+        history.unshift(historyText);
+
+        if (history.length > 5) {
+            history.pop();
+        }
+
+        updateHistoryDisplay(historyElement, history);
+    }
+}
+
+function updateHistoryDisplay(element, history) {
+    element.innerHTML = "";
+    for (let i = 0; i < history.length; i++) {
+        element.innerHTML += `<p class="history-item">${history[i]}</p>`;
+    }
+}
+
+function validateInput(inputElement) {
+    const inputValue = inputElement.value;
+    const validValue = inputValue.replace(/,/g, '.');
+    inputElement.value = validValue;
+}
