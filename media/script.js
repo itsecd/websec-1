62718b861e2ef70afa84b2e8fd91b85d247a05e3
ad-klevtsov <ad.klevtsov@gmail.com>
2023-10-11 02:20:58 +0400

    Functionality fixed

--- media/script.js
+++ media/script.js
@@ -1,11 +1,24 @@
 function buttonClick() {
+    document.getElementById("field1").style.border = "1px dashed red";
+    document.getElementById("field2").style.border = "1px dashed red";
 
     let value1 = document.getElementById("field1").value;
     let value2 = document.getElementById("field2").value;
     let operation = document.getElementById("operation").value;
 
     value1 = parseFloat(value1);
+    if (isNaN(value1)) {
+        document.getElementById("field1").style.border = "2px solid red";
+        alert("Некорректные данные в левой ячейке! Введите число");
+        return;
+    }
+
     value2 = parseFloat(value2);
+    if (isNaN(value2)) {
+        document.getElementById("field2").style.border = "2px solid red";
+        alert("Некорректные данные в правой ячейке! Введите число");
+        return;
+    }
 
     let result = "";
     let previousResult = document.getElementById("result").value;
@@ -22,6 +35,7 @@ function buttonClick() {
             break;
         case "/":
             if (Math.abs(value2 - 0) < Number.EPSILON) {
+                document.getElementById("field2").style.border = "2px solid red";
                 alert("На ноль делить нельзя! Измените операцию или второе число.");
                 return;
             }
@@ -31,7 +45,8 @@ function buttonClick() {
             break;
     }
 
-    result = value1 + " " + operation + " " + value2 + " = " + result + "\n";
+    result = value1 + " " + operation + " " + value2 + " = " + result;
 
-    document.getElementById("result").value = result + previousResult;
+    document.getElementById("previousResult").value = previousResult;
+    document.getElementById("result").value = result;
 }
