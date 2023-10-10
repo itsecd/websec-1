c524ae45ec147775148470fcc20905f9ea34bf72
Admin <Admin@LAPTOP-U4DN8FG5>
2023-10-10 21:34:31 +0400

    Реализована "суровая" валидация

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,27 +1,8 @@
-function error(str) 
-{
-  alert("Value in " + str + " is not a number");
-}
-
 function calculate(){
-    let value1 = document.getElementById("value1").value;
-    let value2 = document.getElementById("value2").value;
+    let value1 = document.getElementById("value1").value || 0;
+    let value2 = document.getElementById("value2").value || 0;
     let operation = document.getElementById("selectOperations");
-    let result = document.getElementById("lastresult").value;
-
-    if(isNaN(value1))
-    {
-      value1.onclick = error("first field");
-      document.getElementById("value1").focus();
-      return
-    }
-    if(isNaN(value2))
-    {
-      value2.onclick = error("second field");
-      document.getElementById("value2").focus();
-      return
-    }
-
+    
     switch (operation.value) {
         case '+':
             result = parseFloat(value1) + parseFloat(value2);
@@ -33,17 +14,16 @@ function calculate(){
             result = parseFloat(value1) * parseFloat(value2);
             break;
         case '/':
-            if (parseFloat(value2) != 0)
-                result = parseFloat(value1) / parseFloat(value2);
-            else
+            if (Math.abs(value2 - 0) < Number.EPSILON)
                 result = "division by zero";
+            else
+                result = parseFloat(value1) / parseFloat(value2);
             break;
         case '^':
             result = parseFloat(value1) ^ parseFloat(value2);
             break;
     }
 
-    if (!isNaN(result))
     result = value1 + " " + operation.value + " " + value2 + " " + "=" + " " + result;
 
     document.getElementById("memory").innerHTML = document.getElementById("lastresult").innerHTML;  
