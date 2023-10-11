c60c345dc5c1e40a1ae45cd79fe5d190b8e4cf68
Стефан Степанов <a1806430@yandex.ru>
2023-10-11 13:58:42 +0400

    Rewrite code from lesson

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 function OnClickHandler(){
-    console.log("OnClickHandler")
+
+    let v1 = document.getElementById("number1").value;
+    let v2 = document.getElementById("number2").value;
+    let op = document.getElementById("operation").value;
+
+    v1 = parseFloat(v1);
+    v2 = parseFloat(v2);
+    
+    console.log(v1, v2, op, typeof v1, typeof v2);
+
+    let result = null;
+
+    if(op ==='+'){
+        result = v1 + v2;
+    }
+    if(op ==='-'){
+        result = v1 - v2;
+    }
+    if(op ==='*'){
+        result = v1 * v2;
+    }
+    if(op ==='/'){
+        if(Math.abs(v2)<Number.EPSILON){
+            result = 'нельзя делить на ноль!';
+        }
+        else{
+            result = v1 / v2;
+        }
+    }
 }
+
+document.getElementById("result").value=result;
