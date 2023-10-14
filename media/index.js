a5bcd9a3731d63e8bf0a2e8a8445cb7cea7d2b7d
T-Marria <t.marria@mail.ru>
2023-10-14 18:08:09 +0400

    done in class

---
+++ media/index.js
@@ -0,0 +1,33 @@
+function buttonClick() {
+    let v1 = document.getElementById("field1").value;
+    let v2 = document.getElementById("field2").value;
+    let op = document.getElementById("operation").value;
+
+    v1 = parseFloat(v1);
+    v2 = parseFloat(v2);
+
+    let result = "";
+
+    if (op === "+") {
+        result = v1 + v2;
+    }
+
+    if (op === "-") {
+        result = v1 - v2;
+    }
+
+    if (op === "*") {
+        result = v1 * v2;
+    }
+
+    if (op === "/") {
+        if (Math.abs(v2) < Number.EPSILON) {
+            result = "На ноль делить нельзя!"
+        }
+        result = v1 / v2;
+    }
+
+    document.getElementById("result").value = result;
+
+    console.log("buttonClick", v1, v2, op, typeof v1, typeof v2);
+}
