a51a14a99eba31163384fef20dbfee42cd29201d
Стефан Степанов <a1806430@yandex.ru>
2023-10-15 20:31:32 +0400

    Done.

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
-function OnClickHandler(){
 
+function OnClickHandler(){
+    document.getElementById("number1").style.backgroundColor='rgb(186,255,200)';
+    document.getElementById("number2").style.backgroundColor='rgb(186,255,200)';
     let v1 = document.getElementById("number1").value;
     let v2 = document.getElementById("number2").value;
     let op = document.getElementById("operation").value;
 
     v1 = parseFloat(v1);
     v2 = parseFloat(v2);
+
+    if (isNaN(v1))
+    {
+        document.getElementById("number1").style.backgroundColor='yellow';
+        alert('Введите число');
+        return;
+    }
+    if (isNaN(v2))
+    {
+        document.getElementById("number2").style.backgroundColor='yellow';
+        alert('Введите число');
+        return;
+    }
     
     console.log(v1, v2, op, typeof v1, typeof v2);
 
     let result = null;
 
-    if(op ==='+'){
-        result = v1 + v2;
+    switch (op) {
+        case '+':
+            result=v1+v2;
+            break;
+        case '-':
+            result=v1-v2;
+            break;
+        case '*':
+            result=v1*v2;
+            break;
+        case '/':
+            if (Math.abs(v2)<Number.EPSILON){
+                alert('Нельзя делить на ноль');
+                document.getElementById("number2").style.backgroundColor='yellow';
+                return;
+            }
+            else
+                result=v1/v2;
+                break;
+        default:
+            break;
     }
-    if(op ==='-'){
-        result = v1 - v2;
-    }
-    if(op ==='*'){
-        result = v1 * v2;
-    }
-    if(op ==='/'){
-        if(Math.abs(v2)<Number.EPSILON){
-            result = 'нельзя делить на ноль!';
-        }
-        else{
-            result = v1 / v2;
-        }
-    }
+    document.getElementById('prevResult').value=document.getElementById('result').value;
+    document.getElementById('result').value=v1+op+v2+'='+result;
 }
 
-document.getElementById("result").value=result;
