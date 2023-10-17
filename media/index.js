4dcd8967ba6276163bc7adace0cab33ece232532
T-Marria <t.marria@mail.ru>
2023-10-18 00:55:47 +0400

    css in progress

--- media/index.js
+++ media/index.js
@@ -5,35 +5,35 @@ function buttonClick() {
 
     number1 = parseFloat(number1);
     number2 = parseFloat(number2);
-    if(isNaN(number1) || isNaN(number2)) {
-        alert("Некорректные данные! Операция может быть выполнена только с числами.")
+    if (isNaN(number1) || isNaN(number2)) {
+        alert("Некорректные данные! Операция может быть выполнена только с числами.");
     }
     else {
-        let result = "";
+        let result;
         if (operation === "+") {
             result = number1 + number2;
         }
-    
+
         if (operation === "-") {
             result = number1 - number2;
         }
-    
+
         if (operation === "*") {
             result = number1 * number2;
         }
-    
+
         if (operation === "/") {
             if (Math.abs(number2) < Number.EPSILON) {
-                alert("На ноль делить нельзя!")
-                result = "(╯°□°)╯︵ ┻━┻"
+                alert("На ноль делить нельзя!");
+                return;
             }
             else {
                 result = number1 / number2;
             }
         }
-    
-        document.getElementById("result").value = result;
+
+        document.getElementById("result").value = number1 + " " + operation + " " + number2 + " = " + result;
     }
-    
+
     console.log(`${number1} (${typeof number1}) ${operation} ${number2} (${typeof number2}) = ${result}`);
 }
