9e148716c4c05f218f271f67eefe18a6dd8e20a5
Horse <Horse@COMPUTER>
2023-10-19 23:36:26 +0300

    Add validate and union class

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-function calculate() {
+function calculate() 
+{
     let selectedElement = document.getElementById("select");
     let number1 = document.getElementById("number1").value;
     let number2 = document.getElementById("number2").value;
     let result;
-
-    switch (selectedElement.value) {
-        case '+':
-            result = parseFloat(number1) + parseFloat(number2);
-            break;
-        case '-':
-            result = parseFloat(number1) - parseFloat(number2);
-            break;
-        case '*':
-            result = parseFloat(number1) * parseFloat(number2);
-            break;
-        case '/':
-            if (Math.abs(number2 - 0) > Number.EPSILON)
-                result = parseFloat(number1) / parseFloat(number2);
-            else
-                result = "Division by zero";
-            break;
+    if (isNaN(number1) || isNaN(number2)) {
+        document.getElementById("result").innerHTML = "Error input data";
+        console.log(document.getElementById("result").value);
     }
-    if (!isNaN(result))
-        result = number1 + " " + selectedElement.value + " " + number2 + " " + "=" + " " + result;
+    else {
+        switch (selectedElement.value) {
+            case '+':
+                result = parseFloat(number1) + parseFloat(number2);
+                break;
+            case '-':
+                result = parseFloat(number1) - parseFloat(number2);
+                break;
+            case '*':
+                result = parseFloat(number1) * parseFloat(number2);
+                break;
+            case '/':
+                if (Math.abs(number2 - 0) > Number.EPSILON)
+                    result = parseFloat(number1) / parseFloat(number2);
+                else
+                    result = "Division by zero";
+                break;
+        }
+        if (!isNaN(result))
+            result = number1 + " " + selectedElement.value + " " + number2 + " " + "=" + " " + result;
 
-    document.getElementById("result").innerHTML = result + "<br/>";
+        document.getElementById("result").innerHTML = result + "<br/>";
+    }
 }
