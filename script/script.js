2b894f209a58374bc17004a4e70c001a89442eb6
Данил Егориков <dveav@yandex.ru>
2023-10-20 12:18:33 +0400

    Fix: field classes, repeated requests. Add HTML validator instead of JS validator

--- script/script.js
+++ script/script.js
@@ -2,63 +2,49 @@ function onButtonClick(event) {
     const input1 = document.getElementById("number1")
     const input2 = document.getElementById("number2")
 
-    input1.classList.remove("highlight");
-    input2.classList.remove("highlight");
-
-    let value1 = document.getElementById("number1").value;
-    let value2 = document.getElementById("number2").value;
+    let value1 = input1.value;
+    let value2 = input2.value;
     let operation = document.getElementById("operation").value;
-
 
     let previousOperation = document.getElementById('prevOperation');
     let currentOperation = document.getElementById('curOperation');
-    let cur = document.getElementById('curOperation').value;
+    let cur = currentOperation.value;
 
-    let result = null;
+    let result = null
+    value1 = parseFloat(value1);
+    value2 = parseFloat(value2);
 
 
-    if (isNaN(value1))
+    if ((!input1.checkValidity() || !input2.checkValidity()))
     {
-        result = `Error! "${value1}" is not a number!`;
-        input1.classList.add("highlight");}
-    else{
-        input1.classList.remove("highlight");
-        if (isNaN(value2))
-        {
-            result = `Error! "${value2}" is not a number!`;
-            input2.classList.add("highlight");}
-        else{
-            input2.classList.remove("highlight");
-            value1 = parseFloat(value1);
-            value2 = parseFloat(value2);
+        result = `Error! Fields are filled in incorrectly`;
+    }else {
 
 
-            switch(operation)
-            {
-                case '+':
-                    result = `${value1} ${operation} ${value2} = ${value1 + value2}`;
-                    break;
-                case '-':
-                    result = `${value1} ${operation} ${value2} = ${value1 - value2}`;
-                    break;
-                case '*':
-                    result = `${value1} ${operation} ${value2} = ${value1 * value2}`;
-                    break;
-                case '/':
-                    if (Math.abs(value2) < Number.EPSILON){
-                        input2.classList.add("highlight");
-                        result = `Error! Division by zero!`;
-                    }
-                    else{
-                        input2.classList.remove("highlight");
-                        result = `${value1} ${operation} ${value2} = ${value1 / value2}`;}
-            }
+        switch (operation) {
+            case '+':
+                result = `${value1} ${operation} ${value2} = ${value1 + value2}`;
+                break;
+            case '-':
+                result = `${value1} ${operation} ${value2} = ${value1 - value2}`;
+                break;
+            case '*':
+                result = `${value1} ${operation} ${value2} = ${value1 * value2}`;
+                break;
+            case '/':
+                if (Math.abs(value2) < Number.EPSILON) {
+                    input2.setCustomValidity("Invalid field.");
+                    result = `Error! Division by zero!`;
+                } else {
+                    result = `${value1} ${operation} ${value2} = ${value1 / value2}`;
+                }
         }
     }
-    if (cur == null){
+    if (cur == null) {
         previousOperation.textContent = ``;
+    } else {
+        previousOperation.textContent = `${cur}`;
     }
-    else{previousOperation.textContent = `${cur}`;}
     currentOperation.textContent = `${result}`;
 
     document.getElementById('curOperation').value = `${result}`;
