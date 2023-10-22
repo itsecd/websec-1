9c325e60c970c6754411633823b4c309f833e25c
Максим Матросов <matrosov.02@bk.ru>
2023-10-22 20:02:13 +0400

    Исправлен код с очной лабораторной работы выполнены дополнительные задания

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,57 @@
 function onClickHandler(){
+
     let v1 = document.getElementById("firstNumber").value;
-    let v2 = document.getElementById("operation").value;
+    let operation = document.getElementById("operation").value;
     let v3 = document.getElementById("secondNumber").value;
-    console.log("onClick" + v1 + v2+ v3);
-    if (v2 === '/')
+    
+    let prev_result = document.getElementById("result").value;
+    document.getElementById("prev_result").value = prev_result;
+
+    v1 = parseFloat(v1)
+    v3 = parseFloat(v3)
+    let result = null
+    let flag = true;
+    if (isNaN(v1))
     {
-        if(Math.abs(v2)<Math.Number.EPSILION()){
-            console.log("dasd");
-        }
+        document.getElementById("firstNumber").style.backgroundColor='orange';
+        flag=false;
     }
+    if (isNaN(v3))
+    {
+        document.getElementById("secondNumber").style.backgroundColor='orange';
+        flag=false;
+    }
+    if(!flag)
+    {
+        document.getElementById("result").value = "Введите только числа";
+        return;
+    }
+    document.getElementById("firstNumber").style.backgroundColor = "aquamarine";
+    document.getElementById("secondNumber").style.backgroundColor = "aquamarine";
+    switch (operation)
+    {
+
+        case "+":
+            result = v1 + v3;
+            break;
+        case "-":
+            result = v1 - v3;
+            break;
+        case "*":
+            result = v1 * v3;
+            break;
+        case "/":
+            if(Math.abs(v3) < Number.EPSILON){
+                document.getElementById("secondNumber").style.backgroundColor='orange';
+                document.getElementById("result").value = "Не не не... На ноль не делить!!!";
+                return;
+            }
+            else{
+                result = v1 / v3;
+            }
+            break;
+    
+    }
+    document.getElementById("result").value = "".concat(v1, " ", operation, " ", v3, " = ", result);
+    
 }
